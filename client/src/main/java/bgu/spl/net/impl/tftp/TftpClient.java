58af1364d9764c8ab3d752130f535ec2e4a3afec
OfirModai <ofir789987@gmail.com>
2024-03-13 17:20:48 +0200

    orginazed

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -12,16 +12,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class TftpClient {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            args = new String[]{"localhost"};
-        }
-
-        if (args.length >= 2) {
-            System.out.println("you must supply one argument: host");
+        if (args.length != 2) {
+            System.out.println("you must supply two argument: host and port");
             System.exit(1);
         }
 
-        BaseClient baseClient = new BaseClient(7777,
+        BaseClient baseClient = new BaseClient(Integer.parseInt(args[1]),
                 new TftpClientProtocol(), new TftpEncoderDecoder());
         baseClient.consume(args[0]);
     }
